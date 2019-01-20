5d1cecaeb9e2c094be4fe5799676312225f7a042
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-01-20 10:57:41 +0200

    done components and css

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './layout/Header';
 import Dashboard from './dashboard/Dashboard';
 import ProjectDetails from './projects/ProjectDetails';
+import SignIn from './auth/SignIn';
+import SignUp from './auth/SignUp';
+import CreateProject from './projects/CreateProject';
 
 class App extends Component {
   render() {
@@ -13,6 +16,9 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Dashboard} />
             <Route path="/project/:id" component={ProjectDetails} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/create" component={CreateProject} />
           </Switch>
         </div>
       </BrowserRouter>
