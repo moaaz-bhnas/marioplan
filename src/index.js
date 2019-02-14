51dd4451cb213c7a6594554f1be57181778880a6
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-02-14 15:15:35 +0200

    1- Installed react-redux-firebase and redux-firebase and used them to interact with firebase API and sync firestore with our redux store

--- src/index.js
+++ src/index.js
@@ -3,17 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux'; // Pass the store into our app
 import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+// We'll use them to interact with Firebase API
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import fbConfig from './config/fbConfig' 
 
-/* applyMiddleware
-- This function could take a list of middlewares.
-- It turns into a store enhancer (enhaces the store with extra functionality, in this case the ability to return a function inside our action creators which can then interact with our database).
-- We can add many store enhancers other than applyMiddleware.
+/* .withExtraArgument
+- You remember that Thunk lets us return a function that pauses the dispatch?
+- Using this method, we can call this function with an extra argument (object in this case)
 */
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, 
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})), // Store enhancer 1
+    reactReduxFirebase(fbConfig), // Store enhancer 2
+    reduxFirestore(fbConfig) // Store enhancer 3
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>, 
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -4,8 +4,18 @@
 3- carry on by calling the dispatch function.
 */
 export const createProject = project => {
-	return (dispatch, getState) => {
-		// Make async call to the databas
+	return (dispatch, getState, {getFirestore}) => { // Destructuring
+		// (1) Initialize getFirestore function and store the result in a variable
+		const firestore = getFirestore();
+		// (2) Access "projects" collection and add a document (project) to it
+		firestore.collection('projects').add({
+			...project,
+			authorFirstName: 'Moaaz',
+			authorLastName: 'Bhnas',
+			authorId: '228',
+			createdAt: new Date()
+		})
+		// Make async call to the database
 		dispatch({ type: 'CREATE_PROJECT', project })
 	}
 }
