c824f587498485b8b90270ac1ea235c3b1946af4
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-02-15 12:20:00 +0200

    updated auth reducer to handle error and success

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,24 @@
-const initState = {};
+const initState = {
+  authError: null
+};
 
 const authReducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case 'LOGIN_SUCCESS':
+      console.log('Login success');
+      return {
+        ...state,
+        authError: null
+      }
+    case 'LOGIN_ERROR':
+      console.log('Login error');
+      return {
+        ...state,
+        authError: 'login error!'
+      }
+    default:
+      return state;
+  }
 }
 
 export default authReducer;
