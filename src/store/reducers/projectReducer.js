498cf6030cbf0c7dd131013f7b3cd7bea9f559d8
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-02-18 11:47:45 +0200

    removed (projects) array from the store sence we're not using it anymore

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,11 +1,4 @@
-const initState = {
-  // You can access this property on the 
-  projects: [
-    {id: '1', title: 'Book Shelves', content: 'Search for books and categorize them.'},
-    {id: '2', title: 'Cat Clicker',  content: 'Click a set of well-known lovable cats.'},
-    {id: '3', title: 'Circles',      content: 'Hover over circles and enjoy the effict.'}
-  ]
-};
+const initState = {};
 
 const projectReducer = (state = initState, action) => {
   switch (action.type) {
